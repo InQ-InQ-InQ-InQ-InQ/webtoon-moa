5c2b4ad558496c6d406d19d3d725946504fed23b
CokeLee777 <akak640@kyonggi.ac.kr>
2022-08-02 18:49:21 +0900

    feat(Set DB): Add MySQL configuration info and connect local database
    MYSQL 설정정보 및 로컬 DB에 연동하였습니다.
    
    #1

---
+++ src/sql/init.sql
@@ -0,0 +1,29 @@
+CREATE TABLE USER (
+  id bigint(20) NOT NULL,
+  identifier varchar(255) NOT NULL,
+  password varchar(255) NOT NULL,
+  username varchar(255) NOT NULL,
+  email varchar(255) NOT NULL,
+);
+
+CREATE TABLE WEBTOON (
+  id bigint(20) PRIMARY KEY,
+  title varchar(255) NOT NULL,
+  author varchar(255) NOT NULL,
+  img_url varchar(255) NOT NULL,
+  web_url varchar(255) NOT NULL,
+  platform_name varchar(255) NOT NULL,
+  genre_name varchar(255) NOT NULL,
+  week int(10) NOT NULL,
+  click_count varchar(255) NOT NULL,
+);
+
+
+CREATE TABLE FAVORITES (
+    id bigint(20) NOT NULL AUTO_INCREMENT,
+    user_id bigint(20) NOT NULL,
+    webtoon_id bigint(20) NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES USER(id),
+    FOREIGN KEY (webtoon_id) REFERENCES WEBTOON(id)
+);
+ 
