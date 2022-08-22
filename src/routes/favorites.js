1e535e2d25a209c9d0a061ed6c1ff6168580a485
CokeLee777 <akak640@kyonggi.ac.kr>
2022-08-22 16:33:54 +0900

    hotfix(favorites): Revise favorite logic
    즐겨찾기 로직을 약간 수정

--- src/routes/favorites.js
+++ src/routes/favorites.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const router = express.Router();
 const db = require('../config/db');
-
-function loginCheck(request, response){
-    if(!request.session.user){
-        response.redirect('/');
-        return;
-    }
-}
+const auth = require('./common/auth');
 
 //즐겨찾기 추가하기 API
 router.post('/', function(request, response){
-    loginCheck(request, response);
-
-    const user_id = request.session.user.id;
+    const user_id = auth.isLogin(request, response);
     const webtoon_id = request.body.webtoon_id;
+
     const sql = `INSERT INTO favorites (user_id, webtoon_id) VALUES (?, ?)`;
     db.query(sql, [user_id, webtoon_id], function(error, result){
         if(error) {
@@ -25,11 +18,34 @@ router.post('/', function(request, response){
     });
 });
 
+// 즐겨찾기 AJAX 코드
+/*
+<script type="text/javascript">
+      $('#favorite_button').click(() => {
+        const webtoon_id = $('#favorite_button').val();
+
+        $.ajax({
+          type: 'POST',
+          url: '/favorites',
+          data: {
+            webtoon_id: webtoon_id
+          },
+          dataType: 'json',
+          success: function(data){
+            $('#favorite_button').css('color', 'red');
+          },
+          error: function(request, status, error){
+            alert(`즐겨찾기 추가를 실패하였습니다.=${error}`)
+          }
+        })
+      })
+    </script>
+*/
+
+
 // 사용자 즐겨찾기 보기 API
 router.get('/list', function(request, response){
-    loginCheck(request, response);
-
-    const user_id = request.session.user.id;
+    const user_id = auth.isLogin(request, response);
 
     const sql = `SELECT * FROM webtoon as w INNER JOIN favorites as f on w.id = f.webtoon_id WHERE f.user_id = ?`;
     db.query(sql, user_id, function(error, favorites){
