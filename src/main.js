ccab6bac58743fb89c1d9dfa745436b77fe85b02
Subin Jo <sbjo1202@naver.com>
2022-08-24 22:36:29 +0900

    메인 페이지 제작중

--- src/main.js
+++ src/main.js
@@ -1,112 +1,102 @@
-// const sortby = document.querySelector('.sortby');
+let today = new Date();
+let week = today.getDay();  // 요일
+var arrWeek = new Array(); // 요일 array
+var arrPlatform = new Array();  // 플랫폼 array
 
-// function select(ulEl, liEl) {
-//   Array.from(ulEl.children).forEach(
-//     v => v.classList.remove('selected')
-//   )
-//   if(liEl) liEl.classList.add('selected');
-// }
+week -= 1
 
-// sortby.addEventListener('click', e => {
-//   const selected = e.target;
-//   select(sortby, selected);
-// })
+if (week == 0) {
+  week = 6
+}
 
-// const clickedClass = "clicked"
-
-// function click_p() {
-
-//   const a = document.querySelector(".sortby .sort_popular")
-
-//   a.classList.toggle(clickedClass);
-// }
-
-// function click_c() {
-//   const clickedClass = "clicked"
-
-// }
-
-// function click_w() {
-//   const clickedClass = "clicked"
-
-// }
-
-
-
-
-
-// var sort = document.getElementsByClassName("sort");
-
-// function handleClick(event) {
-//   console.log(event.target);
-//   console.log(event.target.classList);
-
-//   if (event.target.classList[1] === "clicked") {
-//     event.target.classList.remove("clicked");
-//   } else {
-//     for (var i = 0; i < sort.length; i++) {
-//       sort[i].classList.remove("clicked");
-//     }
-//     event.target.classList.add("clicked");
-//   }
-// }
-
-// function init() {
-//   for (var i = 0; i < sort.length; i++) {
-//     sort[i].addEventListener("click", handleClick);
-//   }
-// }
-
-// init();
-
-
-// var week = document.getElementsByClassName("w");
+// 오늘 날짜 색 반전
 // console.log(week);
+$(document).ready(function() {
+  $('.week-button').ready(function(index){
+    $('.week-button[week-button-index=' + week + ']').addClass('click-button');
+    $('.week-button[week-button-index!=' + week + ']').removeClass('click-button');
+    arrWeek.push(week)
+    // console.log(arrWeek)
+  });
+});
 
-// for (var i = 0; i < week.length; i++) {
-//   week[i].addEventListener('click', function(){
-//     for (var j = 0; j < week.length; j++) {
-//       week[j].getElementsByClassName.color = "red";
-//     }
-//     this.style.color = "green";
-//   })
-// }
+// 플랫폼 리스트 초기화
+$(document).ready(function() {
+  var c = $('input[type=radio][name="chk_platform"]:checked').val();
+  arrPlatform.push(c)
+  // console.log(arrPlatform)
+});
 
-
-
-// var week = document.getElementsByClassName("w");
-
-// function handleClick(event) {
-
-//   console.log(event.target);
-
-//   console.log(event.target.classList);
-
-//   if (event.target.classList[1] === "clicked") {
-//     event.target.classList.remove("clicked");
-//   } else {
-//     for (var i = 0; i < week.length; i++) {
-//       week[i].classList.remove("clicked");
-//     }
-
-//     event.target.classList.add("clicked");
-//   }
-//   console.log(event.target.classList);
-
-// }
-
-// function init() {
-//   for (var i = 0; i < week.length; i++) {
-//     week[i].addEventListener("click", handleClick);
-//   }
-// }
-
-// init();
-
+// 클릭한 요일 색 반전 및 선택된 요일을 리스트에 저장, 요일과 플랫폼 정보 가져오기
 $('.week-button').each(function(index){
+  // console.log(index)
+
   $(this).attr('week-button-index', index);
 }).click(function(){
   var index = $(this).attr('week-button-index');
+  arrWeek.push(parseInt(index))
+  // console.log(arrWeek)
+  // console.log(index)
+
   $('.week-button[week-button-index=' + index + ']').addClass('click-button');
   $('.week-button[week-button-index!=' + index + ']').removeClass('click-button');
+
+  // 요일, 플랫폼 정보 가져오기
+  let check_week = parseInt(arrWeek[arrWeek.length - 1]);
+  let check_platform = arrPlatform[arrPlatform.length - 1];
+
+  check_week = settingWeek(check_week)
+  console.log(check_week, check_platform)
+  call(check_week, check_platform);
 });
+
+// 하트 토글
+$(function() {
+  $(".heart").on("click", function() {
+    $(this).toggleClass("is-active");
+  });
+});
+
+// 선택된 플랫폼을 리스트에 저장, 요일과 플랫폼 정보 가져오기
+$('input[type=radio][name="chk_platform"]').click(function() {
+  var c = $('input[type=radio][name="chk_platform"]:checked').val();
+  arrPlatform.push(c)
+  // console.log(arrPlatform)
+
+  // 요일, 플랫폼 정보 가져오기
+  let check_week = parseInt(arrWeek[arrWeek.length - 1]);
+  let check_platform = arrPlatform[arrPlatform.length - 1];
+
+  check_week = settingWeek(check_week)
+  console.log(check_week, check_platform)
+  call(check_week, check_platform);
+});
+
+// 요일, 플랫폼 정보를 토대로 GET 요청
+function call(check_week, check_platform) {
+  var day = check_week;
+  var platform = check_platform;
+  var page = 1;
+
+  var webtoonData = new Array();
+
+  $.ajax({
+    type: "GET",
+    url: "/webtoon/list/" + day + "/" + platform + "?page=" + page,
+    // url: "https://jsonplaceholder.typicode.com/users",
+    dataType: "json",
+    success: function(data) {
+      console.log(data);
+      webtoonData.push(data);
+      console.log(webtoonData);
+    }
+
+  });
+
+  // 여기에 포문 써서
+  // 어레이 리스트 webtoonData에 담겨 있는 정보들 하나씩 뽑아내며 웹툰 카드 쭈르륵 만들어주기(?)
+}
+
+function settingWeek(check_week) {
+  return check_week += 1
+}
