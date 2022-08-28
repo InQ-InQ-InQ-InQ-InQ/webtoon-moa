d1d8b8d1de2c41ba9a289f3bb1c378582612db31
Subin Jo <sbjo1202@naver.com>
2022-08-28 20:19:46 +0900

    db

--- src/config/db.js
+++ src/config/db.js
@@ -1,7 +1,7 @@
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
     password: '11111111',
     database: 'webtoon-moa',
